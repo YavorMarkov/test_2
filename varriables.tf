8299c6a050b8a059bb7832df5868e4b3d3dc8707
YavorMarkov <76599518+YavorMarkov@users.noreply.github.com>
2024-07-13 19:40:31 +0300

    ***

--- varriables.tf
+++ varriables.tf
@@ -27,7 +27,3 @@ variable "GOOGLE_CREDENTIALS" {
     sensitive   = true
 }
 
-output "instance_ip" {
-    description = "The public IP of the instance"
-    value       = google_compute_instance.enhanced_vm.network_interface[0].access_config[0].nat_ip
-}
