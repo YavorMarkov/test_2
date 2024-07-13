bc8fd7d04792be6b58e1badbef165498d3770be8
YavorMarkov <76599518+YavorMarkov@users.noreply.github.com>
2024-07-13 19:34:36 +0300

    ***

--- varriables.tf
+++ varriables.tf
@@ -1,3 +1,4 @@
+
 variable "project_id" {
     description = "The Google Cloud project ID"
     type        = string
@@ -8,10 +9,25 @@ variable "region" {
     type        = string
 }
 
+variable "zone" {
+    description = "The zone to deploy resources in"
+    type        = string
+    default     = "us-central1-f"
+}
+
+variable "image" {
+    description = "The image to use for the boot disk"
+    type        = string
+    default     = "debian-cloud/debian-11"
+}
+
 variable "GOOGLE_CREDENTIALS" {
     description = "The JSON credentials for Google Cloud"
     type        = string
     sensitive   = true
 }
 
-
+output "instance_ip" {
+    description = "The public IP of the instance"
+    value       = google_compute_instance.enhanced_vm.network_interface[0].access_config[0].nat_ip
+}
