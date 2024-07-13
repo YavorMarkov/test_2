6ff6eeeab6443484909a219b7bbe1c968bad98ea
YavorMarkov <76599518+YavorMarkov@users.noreply.github.com>
2024-07-13 19:38:51 +0300

    ****

--- output.tf
+++ output.tf
@@ -1,4 +1,4 @@
-output "instance_ip" {
-    description = "The public IP address of the Google Compute Engine instance"
-    value       = google_compute_instance.enhanced_vm.network_interface[0].access_config[0].nat_ip
-}
+# output "instance_ip" {
+#     description = "The public IP address of the Google Compute Engine instance"
+#     value       = google_compute_instance.enhanced_vm.network_interface[0].access_config[0].nat_ip
+# }
